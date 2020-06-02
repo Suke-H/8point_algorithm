f829b715593f4360983fd456fbf36963d04ee0ad
suke0806 <hondii1997@gmail.com>
2020-06-02 17:31:38 +0900

    エピポーラ線描画

--- 8point.py
+++ 8point.py
@@ -3,9 +3,9 @@ import numpy.linalg as LA
 
 from draw_image import draw_lines
 
-width = 768
-height = 432
- 
+width = 1108
+height = 1477
+
 uvmat = np.array([[207, 193, 312, 256],
                     [251, 279, 432, 330],
                     [330, 279, 508, 290],
@@ -33,14 +33,23 @@ for i in range(8):
 
 # matを特異値分解
 U, D, V = LA.svd(mat)
+#最小の固有値を探す
+print("D=",D)
+print("V=",V)
+print(V.shape)
+print(V[8])
 # Vから固有値最小の固有ベクトルを取り出してFにする
-F = V[8].reshape((3, 3)).T
+F = V[8].reshape((3, 3))
+print("F=",F)
 
 ### 以下の処理はコメントにしても大丈夫、あると精度がよくなる
 # Fのランクを2にする
+'''
 U, D, V = LA.svd(F)
 F = np.dot(U, np.diag([D[0], D[1], 0]))
 F = np.dot(F, V.T)
+print("F'=",F)
+print("F_rank=",LA.matrix_rank(F))
 ### おわり
 
 # Nにより正規化
@@ -49,13 +58,12 @@ N = np.array([[2/width, 0, -1],
             [0, 0, 1]])
 F = np.dot(N.T, F)
 F = np.dot(F, N)
-
 print(F)
-
+'''
 # Fの固有値は[1,0,0]になる
 value, vec = LA.eig(F)
 
-print(value)
+print("Fの固有値=", value)
 
 # F = value.reshape((3, 3))
 # F = []
@@ -71,20 +79,16 @@ for i in range(8):
     # x1 = [u1, u2, 1], x2 = [u2, v2, 1]
     x1 = np.array([uvmat[i, 0], uvmat[i, 1], 1.0])
     x2 = np.array([uvmat[i, 2], uvmat[i, 3], 1.0])
-    #print(x1, x2)
 
     # Nにより正規化
+    '''
     x1 = np.dot(N, x1.T)
     x2 = np.dot(N, x2.T)
-    # x3 = np.array([width/2, height/2, 1])
-    # x3 = np.dot(N, x3.T)
-    # print(x1, x2, x3)
-
+    '''
     # x2^T * F * x1 = 0 のチェック
-    # a = np.dot(x2.T, F)
-    # print(a, np.dot(a, x1))
-    b = np.dot(F.T, x2)
-    print(b, np.dot(x1.T, b))
+    a = np.dot(x2.T, F)
+    print(np.dot(a, x1))
 
-print("="*50)
-draw_lines(F, uvmat)
+width = 768
+height = 432
+draw_lines(F, width, height, uvmat)
--- draw_image.py
+++ draw_image.py
@@ -12,74 +12,80 @@ def line2d(a, b):
 
     return x, y            
 
-def draw_lines(F, uv_mat):
-    width = 768
-    height = 432
-    N = np.array([[2/width, 0, -1],
-                [0, 2/height, -1],
-                [0, 0, 1]])
+def draw_lines(F, width, height, uv_mat):
 
     n = uv_mat.shape[0]
 
+    # 対応点の座標
     x1_set = np.array([[uv_mat[i, 0], uv_mat[i, 1], 1] for i in range(n)])
     x2_set = np.array([[uv_mat[i, 2], uv_mat[i, 3], 1] for i in range(n)])
 
-    for i in range(n):
-        # Nにより正規化
-        x1 = np.dot(N, x1_set[i].T)
-        x2 = np.dot(N, x2_set[i].T)
-        # x1 = x1_set[i].T
-        # x2 = x2_set[i].T
+    # #figure()でグラフを表示する領域をつくり，figというオブジェクトにする．
+    # fig = plt.figure()
+
+    # #add_subplot()でグラフを描画する領域を追加する．引数は行，列，場所
+    # ax1 = fig.add_subplot(1, 2, 1)
+    # ax2 = fig.add_subplot(1, 2, 2)
+
+    for x1, x2 in zip(x1_set, x2_set):
+
+        # 画像枠
+        line1 = line2d([0,0], [0,height])
+        line2 = line2d([0,height], [width,height])
+        line3 = line2d([width,height], [width,0])
+        line4 = line2d([width,0], [0,0]) 
+        plt.plot(line1[0],line1[1],marker=".",linestyle="None",color="black")
+        plt.plot(line2[0],line2[1],marker=".",linestyle="None",color="black")
+        plt.plot(line3[0],line3[1],marker=".",linestyle="None",color="black")
+        plt.plot(line4[0],line4[1],marker=".",linestyle="None",color="black")
+
         print(x1, x2)
 
+        # F x1 = [a, b, c]よりエピポーラ線のパラメータ算出
         pram1 = np.dot(F.T, x2)
         pram2 = np.dot(F, x1)
 
-        a1, b1, c1 = pram1
-        print(a1, b1, c1)
+        a, b, c = pram1
 
-        # a2, b2, c2 = pram2
+        line = np.empty((0, 2))
 
-        # start1 = [-c1/a1, -c1/b1]
-        # end1 = [-(b1*height+c1)/a1, -(a1*width+c1)/b1]
+        # x=0, w, y=0, hとの4交点のうち、画像枠内にある2点を選択
+        u1 = -c/a
+        u2 = -(b*height+c)/a
+        v1 = -c/b
+        v2 = -(a*width+c)/b
 
-        # start2 = [-c2/a2, -c2/b2]
-        # end2 = [-(b2*height+c2)/a2, -(a2*width+c2)/b2]
+        if 0 <= u1 <= width:
+            line = np.append(line, np.array([[u1, 0]]), axis=0)
+            print(1)
 
-        # x_cross = [-c1/a1, 0]
-        # y_cross = [0, -c1/b1]
-        # line1 = line2d(x_cross, y_cross)
-        # print(x_cross, y_cross)
+        if 0 <= u2 <= width:
+            line = np.append(line, np.array([[u2, height]]), axis=0)
+            print(2)
 
-        # print("="*50)
+        if 0 <= v1 <= height:
+            line = np.append(line, np.array([[0, v1]]), axis=0)
+            print(3)
 
-        # plt.plot(line1[0],line1[1],marker=".",color="yellow")
-        # plt.plot(x_cross, y_cross,marker="o",color="red")
+        if 0 <= v2 <= height:
+            line = np.append(line, np.array([[width, v2]]), axis=0)
+            print(4)
 
-        p = np.array([[(b1-c1)/a1, -1], 
-                        [-(b1+c1)/a1, 1],
-                        [-1, (a1-c1)/b1],
-                        [1, -(a1+c1)/b1]])
+        print(line)
 
-        plt.plot(p[:, 0], p[:, 1],linestyle="None", marker="o",color="blue")
+        # x=0, w, y=0, hとの4交点のうち、画像枠内にある2点を選択
+        plt.plot(line[:, 0], line[:, 1], marker="o",color="blue")
+        plt.show()
+        plt.close()
 
-    line1 = line2d([-1,-1], [-1,1])
-    line2 = line2d([-1,1], [1,1])
-    line3 = line2d([1,1], [1,-1])
-    line4 = line2d([1,-1], [-1,-1])
-
+    # 画像枠
+    line1 = line2d([0,0], [0,height])
+    line2 = line2d([0,height], [width,height])
+    line3 = line2d([width,height], [width,0])
+    line4 = line2d([width,0], [0,0]) 
     plt.plot(line1[0],line1[1],marker=".",linestyle="None",color="black")
     plt.plot(line2[0],line2[1],marker=".",linestyle="None",color="black")
     plt.plot(line3[0],line3[1],marker=".",linestyle="None",color="black")
     plt.plot(line4[0],line4[1],marker=".",linestyle="None",color="black")
 
     plt.show()
-
-
-
-
-
-
-
-
-    
